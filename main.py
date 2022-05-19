7c889cb24ac06cba2ad6934991dd1095378da30a
Pedro Lima <contact.pedrolima@gmail.com>
2022-05-19 16:15:27 -0300

    added play/pause buttom

--- main.py
+++ main.py
@@ -12,11 +12,13 @@ gray = (128, 128, 128)
 green = (0, 255, 0)
 gold = (212, 175, 55)
 blue = (0, 255, 255)
+dark_gray = (50, 50, 50)
 
 
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 pygame.display.set_caption("Drum Machine")
 label_font = pygame.font.Font('freesansbold.ttf', 30)
+medium_font = pygame.font.Font('freesansbold.ttf', 24)
 
 fps = 60
 timer = pygame.time.Clock()
@@ -106,6 +108,17 @@ while run:
     timer.tick(fps)
     screen.fill(black)
     boxes = draw_grid(clicked, active_beat)
+    # lower buttons
+    play_pause = pygame.draw.rect(screen, gray, [50, HEIGHT - 150, 200, 100], 0, 5)
+    play_text = label_font.render('Play/Pause', True, white)
+    screen.blit(play_text, (70, HEIGHT - 130))
+
+    if playing:
+        play_text2 = medium_font.render('Playing', True, dark_gray)
+    else:
+        play_text2 = medium_font.render('Paused', True, dark_gray)
+    screen.blit(play_text2, (70, HEIGHT - 100))
+
     if beat_changed:
         play_notes()
         beat_changed = False
@@ -118,6 +131,12 @@ while run:
                 if boxes[i][0].collidepoint(event.pos):
                     coords = boxes[i][1]
                     clicked[coords[1]][coords[0]] *= -1
+        if event.type == pygame.MOUSEBUTTONUP:
+            if play_pause.collidepoint(event.pos):
+                if playing:
+                    playing = False
+                elif not playing:
+                    playing = True
 
     beat_Lenght = 3600 // bpm
 
